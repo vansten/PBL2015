8805772f72431b6328b589e64499ef3afc286e39
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2015-03-13 10:41:15 +0100

    Added singleton base class. You can now make classes that are singletons in simply way. Just use class Name : Singleton<Name> {} and that's all.

---
+++ TrashSoup/TrashSoup/TrashSoup/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrashSoup
+{
+    class Singleton <T> where T : new()
+    {
+        private static T m_instance;
+        private static object locker = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                if(m_instance == null)
+                {
+                    lock (locker)
+                    {
+                        if(m_instance == null)
+                        {
+                            m_instance = new T();
+                        }
+                    }
+                }
+
+                return m_instance;
+            }
+        }
+    }
+}
