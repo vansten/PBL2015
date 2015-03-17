6f3549d1ce463fbd7ce7e17fea4e6b8ce3789878
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2015-03-17 17:36:16 +0100

    Debug class added. You can now just write Debug.Log("message") and you should see your message on additional console window

---
+++ TrashSoup/TrashSoup/TrashSoup/Engine/Debug.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TrashSoup.Engine
+{
+    class Debug
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
