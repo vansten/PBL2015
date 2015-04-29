46a7a3001e1533f5674c88074ac7e7f588c47a93
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2015-04-30 01:07:35 +0200

    Fix in raycasting

--- TrashSoup/TrashSoup/TrashSoup/Engine/Raycast.cs
+++ TrashSoup/TrashSoup/TrashSoup/Engine/Raycast.cs
@@ -56,7 +56,7 @@ namespace TrashSoup.Engine
 
                 foreach (Collider collider in allColliders)
                 {
-                    if(!collider.MyObject.Enabled)
+                    if(!collider.MyObject.Enabled || collider.IsTrigger)
                     {
                         continue;
                     }
