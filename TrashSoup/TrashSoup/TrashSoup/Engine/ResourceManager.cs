e4f5b919892e5a4399363140d2a2936b96e65792
wikakoz <wikakoz@wp.pl>
2015-05-23 22:07:06 +0200

    hammer

--- TrashSoup/TrashSoup/TrashSoup/Engine/ResourceManager.cs
+++ TrashSoup/TrashSoup/TrashSoup/Engine/ResourceManager.cs
@@ -247,7 +247,7 @@ namespace TrashSoup.Engine
 
             //Wika i Kasia testowanie modeli
             GameObject awsomeTest = new GameObject(8, "testground");
-            awsomeTest.MyTransform = new Transform(awsomeTest, new Vector3(5.0f, 5.0f, 5.0f), new Vector3(0.0f, 0.0f, 1.0f), new Vector3(MathHelper.PiOver2, 0.0f, 0.0f), 0.01f);
+            awsomeTest.MyTransform = new Transform(awsomeTest, new Vector3(-12.0f, 1.0f, -5.0f), new Vector3(0.0f, 0.0f, 1.0f), new Vector3(MathHelper.PiOver2, 0.0f, 0.0f), 1.0f);
             awsomeTest.Components.Add(new CustomModel(awsomeTest, new Model[] { Models["Models/Weapons/Knife/Knife"], null, null }, awsomeTestMats));
             awsomeTest.MyCollider = new BoxCollider(awsomeTest, false);
 
