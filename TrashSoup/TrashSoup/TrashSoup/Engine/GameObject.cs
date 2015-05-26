dea47542f748e65d429bcf0011427649ecc8ecfa
trolandus <johnnybe.beczcki@gmail.com>
2015-05-26 17:51:50 +0200

    Forgot to comment drawing colliders xD

--- TrashSoup/TrashSoup/TrashSoup/Engine/GameObject.cs
+++ TrashSoup/TrashSoup/TrashSoup/Engine/GameObject.cs
@@ -211,7 +211,7 @@ namespace TrashSoup.Engine
 #if DEBUG
                 if (this.MyCollider != null)
                 {
-                   this.MyCollider.Draw(cam, effect, gameTime);
+                   //this.MyCollider.Draw(cam, effect, gameTime);
                 }
 
 #endif
